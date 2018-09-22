c5661f429d931333cd43d9aa443dbf704b4a173c
LorinStewart <stewartalorin@gmail.com>
2018-09-22 16:57:36 -0700

    removed number generator as it was incorrect for the project

--- js/app.js
+++ js/app.js
@@ -1,8 +1,36 @@
 'use strict';
 
-var minMax = function() {
-  var randomNum = Math.floor((Math.random() * 100) +1);
-  console.log(randomNum);
+var firstAndPike = {
+  minCustomer: 23,
+  maxCustomer: 65,
+  averageSale: 6.3,
 };
+console.log(firstAndPike);
 
-minMax();
+var seaTac = {
+  minCustomer: 3,
+  maxCustomer: 24,
+  averageSale: 1.2,
+};
+console.log(seaTac);
+
+var seattleCenter = {
+  minCustomer: 11,
+  maxCustomer: 38,
+  averageSale: 3.7,
+};
+console.log(seattleCenter);
+
+var capitolHill = {
+  minCustomer: 20,
+  maxCustomer: 38,
+  averageSale: 2.3,
+};
+console.log(capitolHill);
+
+var alki = {
+  minCustomer: 2,
+  maxCustomer: 16,
+  averageSale: 4.6,
+};
+console.log(alki);
