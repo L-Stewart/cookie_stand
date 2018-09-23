5f5dfa03120c4c34ed8da01daba837c619590ee5
LorinStewart <stewartalorin@gmail.com>
2018-09-22 17:56:27 -0700

    not sure what im doing anymore

--- js/app.js
+++ js/app.js
@@ -1,36 +1,46 @@
 'use strict';
+var time = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+// //Random number generator
+// var minMax = function() {
+//   var randomNum = Math.floor((Math.random() * 100) +1);
+//   console.log(randomNum);
+// };
+// minMax();
 
 var firstAndPike = {
-  minCustomer: 23,
-  maxCustomer: 65,
+  min: 23,
+  max: 65,
   averageSale: 6.3,
+  random: function(min, max) {
+    Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 };
 console.log(firstAndPike);
 
-var seaTac = {
-  minCustomer: 3,
-  maxCustomer: 24,
-  averageSale: 1.2,
-};
-console.log(seaTac);
+// var seaTac = {
+//   min: 3,
+//   max: 24,
+//   averageSale: 1.2,
+// };
+// console.log(seaTac);
 
-var seattleCenter = {
-  minCustomer: 11,
-  maxCustomer: 38,
-  averageSale: 3.7,
-};
-console.log(seattleCenter);
+// var seattleCenter = {
+//   min: 11,
+//   max: 38,
+//   averageSale: 3.7,
+// };
+// console.log(seattleCenter);
 
-var capitolHill = {
-  minCustomer: 20,
-  maxCustomer: 38,
-  averageSale: 2.3,
-};
-console.log(capitolHill);
+// var capitolHill = {
+//   min: 20,
+//   max: 38,
+//   averageSale: 2.3,
+// };
+// console.log(capitolHill);
 
-var alki = {
-  minCustomer: 2,
-  maxCustomer: 16,
-  averageSale: 4.6,
-};
-console.log(alki);
+// var alki = {
+//   min: 2,
+//   max: 16,
+//   averageSale: 4.6,
+// };
+// console.log(alki);
